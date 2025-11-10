0ea842150a6f608ac0ac97f57a11f620c6a87b94
Sara <12196276+SaravananWD@users.noreply.github.com>
2025-11-10 15:12:40 +0530

    Daily Challenge 47

--- Daily Challenges 2025/11 November/Nov 10 - Extension Extractor/Extension Extractor.js
+++ Daily Challenges 2025/11 November/Nov 10 - Extension Extractor/Extension Extractor.js
@@ -8,6 +8,7 @@ Link: https://www.freecodecamp.org/learn/daily-coding-challenge/2025-11-10
 
 function getExtension(filename) {
   const lastIndex = filename.lastIndexOf(".");
+
   return lastIndex >= 0 && filename[lastIndex + 1] !== undefined
     ? filename.slice(lastIndex + 1)
     : "none";
