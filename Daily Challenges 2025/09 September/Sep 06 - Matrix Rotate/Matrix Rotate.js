fee9e49bf48c4cdf9d2a2424425bfd7e7e020748
Sara <12196276+SaravananWD@users.noreply.github.com>
2025-11-28 09:54:32 +0530

    Daily Challenge 85

---
+++ Daily Challenges 2025/09 September/Sep 06 - Matrix Rotate/Matrix Rotate.js
@@ -0,0 +1,30 @@
+/*
+Challenge #85
+
+Date: September 7, 2025
+Name: Matrix Rotate
+Link: https://www.freecodecamp.org/learn/daily-coding-challenge/2025-09-06
+*/
+
+function rotate(matrix) {
+  const rows = matrix.length;
+  const cols = matrix[0]?.length;
+
+  const rotate = [];
+  for (let c = 0; c < cols; c++) {
+    const newRow = [];
+    for (let r = rows - 1; r >= 0; r--) {
+      newRow.push(matrix[r][c]);
+    }
+    rotate.push(newRow);
+  }
+
+  return rotate;
+}
+
+console.log(
+  rotate([
+    [1, 2],
+    [3, 4],
+  ])
+);
